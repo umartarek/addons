1035a1099357c0c088d52de2707d54258c29df62
umartarek <umartarekbusiness@gmail.com>
2024-11-24 14:49:05 +0200

    Delete pp_addon directory

--- pp_addon/report/datev/datev.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016, magdyabouelatta and contributors
-// For license information, please see license.txt
-/* eslint-disable */
-
-frappe.query_reports["DATEV"] = {
-	"filters": [
-
-	]
-};
